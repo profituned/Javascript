5e1484f32173261eaf89b62e35abd32fe2c59e70
profituned <146980066+profituned@users.noreply.github.com>
2024-01-22 18:55:45 +0000

    Javascript course

---
+++ index.js
@@ -0,0 +1,89 @@
+// console.log("Hello World")
+
+
+// let value = 3; 
+
+// let negValue = -value
+// console.log(negValue)
+// console.log('hello')
+
+
+// let str1 = "hello" 
+// let str2 = "Hassan"
+
+
+// let str3 = str1 + str2
+
+// console.log(str3)
+
+// let num1 = "1"
+// let num2 = 2
+// console.log(num1 + num2)
+
+
+// console.log("1" + 2)
+
+
+// console.log( (3 + 4) * 5 % 3)
+
+
+// console.log(true)
+
+
+// console.log(+"")
+
+
+// let num1, num2, num3
+
+// num1 = num2 = num3 = 2 + 2
+
+
+// let gameCounter = 100
+
+// gameCounter++;
+// console.log(gameCounter)
+
+
+// console.log(2 > 1)
+
+// console.log("2" > 1)
+
+// console.log(2 > "1")
+
+
+
+// console.log(null > 0)
+// console.log(null ==0)
+// console.log(null >= 0)
+
+
+
+
+
+
+
+
+
+// 7 tpes: String, Number, Boolean, null, undefined, sybmol  BigInt
+
+
+
+// 
+
+
+const score = 100
+const scoreValue = 100.3
+const isLoggedIn = false;
+const outsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+console.log(id === anotherId)
+
+const bigNumber = 34214324312432142142424321
+console.log(typeof bigNumber)
+
+
+
+
